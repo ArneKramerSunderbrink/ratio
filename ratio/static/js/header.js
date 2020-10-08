6addb40b6d49663c87fe306124dfe1de685516a7
ArneKramerSunderbrink <herr-kramer-sunderbrink@t-online.de>
2020-10-08 18:22:06 +0200

    finished checkbox in menu

--- ratio/static/js/header.js
+++ ratio/static/js/header.js
@@ -12,20 +12,44 @@ $(function() {
   })
 })
 
-// finished checkbox
-$(function() {
-  $('input#finished').bind('change', function() {
-    var finished = $(this).is(':checked');
+// finished checkboxes
+function toggle_finished() {
+  var id = this.id;
+  var finished = $(this).is(':checked');
 
+  alert(id);
+
+  if (id == 'finished') {
+    subgraph_id = $SUBGRAPH_ID;
+  } else {
+    subgraph_id = parseInt(id.substring(9));
+  }
+
+  alert(subgraph_id);
+
+  // change checkbox in the subgraph menu
+  if (finished) {
+    $("label[for='finished-"+subgraph_id+"']").children('i').removeClass().addClass('fa fa-check-square');
+  } else {
+    $("label[for='finished-"+subgraph_id+"']").children('i').removeClass().addClass('fa fa-square');
+  }
+
+  // change checkbox in the header
+  if (subgraph_id == $SUBGRAPH_ID) {
     if (finished) {
       $("label[for='finished']").children('i').removeClass().addClass('fa fa-check-square');
     } else {
       $("label[for='finished']").children('i').removeClass().addClass('fa fa-square');
     }
+  }
 
-    $.getJSON($SCRIPT_ROOT + '_set_finished', {subgraph_id: $SUBGRAPH_ID, finished: finished})
-    .fail(function() { alert('getJSON request failed!'); });
+  $.getJSON($SCRIPT_ROOT + '_set_finished', {subgraph_id: subgraph_id, finished: finished})
+  .fail(function() { alert('getJSON request failed!'); });
 
-    return false;
-  });
+  return false;
+}
+
+$(function() {
+  $('input#finished').bind('change', toggle_finished);
+  $('div#subgraph-list').find(':checkbox').bind('change', toggle_finished);
 });
