ca879b576423ec663d1b4e031725311a2376dc0e
ArneKramerSunderbrink <herr-kramer-sunderbrink@t-online.de>
2020-11-10 12:40:37 +0100

    browser test header

--- tests/test_browser.py
+++ tests/test_browser.py
@@ -3,12 +3,8 @@ import pytest
 from flask import url_for
 from selenium.webdriver import Firefox
 from selenium.webdriver.firefox.options import Options
-
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support.expected_conditions import presence_of_element_located
-from selenium.webdriver.support.expected_conditions import url_to_be
 
 
 @pytest.fixture
@@ -17,6 +13,7 @@ def browser():
     options = Options()
     options.headless = True
     driver = Firefox(options=options, executable_path=driver_path)
+    driver.implicitly_wait(10)
 
     yield driver
 
@@ -24,16 +21,27 @@ def browser():
 
 
 @pytest.mark.usefixtures('live_server')
-def test_selenium(browser):  # todo this is just a temporary file to test how to run selenium
-    wait = WebDriverWait(browser, 10)
-
+def test_header(browser):
     browser.get(url_for('tool.index', _external=True))
-    wait.until(url_to_be(url_for('auth.login', next_url=url_for('tool.index'), _external=True) + '%3F'))
-    content = browser.page_source
-
-    assert 'Log In' in content
-    #browser.find_element(By.NAME, 'q').send_keys('cheese' + Keys.RETURN)
-    #first_result = wait.until(presence_of_element_located((By.CSS_SELECTOR, 'h3>div')))
-    #print(first_result.get_attribute('textContent'))
-
-    # todo run server local and visit my own side
+    browser.find_element(By.ID, 'username').send_keys('test')
+    browser.find_element(By.ID, 'password').send_keys('test')
+    browser.find_element(By.ID, 'password').send_keys(Keys.ENTER)
+    # logged and redirected?
+    assert browser.find_element(By.LINK_TEXT, 'Log Out')
+    browser.find_element(By.LINK_TEXT, 'subgraph1').click()
+    # redirected to /1?
+    assert browser.find_element(By.ID, 'subgraph-name').text == 'subgraph1'
+    # test toggling the overlay
+    browser.find_element(By.ID, 'subgraph-name').click()
+    assert browser.find_element(By.ID, 'overlay').is_displayed()
+    browser.find_element(By.ID, 'subgraph-name').click()
+    assert not browser.find_element(By.ID, 'overlay').is_displayed()
+    browser.find_element(By.ID, 'subgraph-name').click()
+    # test toggling the finished checkbox
+    browser.find_element(By.CSS_SELECTOR, 'header > label[for="finished"]').click()
+    assert browser.find_element(By.CSS_SELECTOR, 'div#subgraph-list input#finished-1').is_selected()
+    browser.find_element(By.CSS_SELECTOR, 'div#subgraph-list label[for="finished-1"]').click()
+    assert not browser.find_element(By.CSS_SELECTOR, 'header > input#finished').is_selected()
+    # log out
+    browser.find_element(By.ID, 'close-overlay').click()
+    browser.find_element(By.LINK_TEXT, 'Log Out').click()
