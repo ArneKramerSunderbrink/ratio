37258d1120fa2bc37affb293d20239000c69b573
Arne Kramer-Sunderbrink <herr-kramer-sunderbrink@t-online.de>
2021-03-10 17:47:41 +0100

    flip functions are global again because turns out they have to

--- ratio/static/js/flip_pattern.js
+++ ratio/static/js/flip_pattern.js
@@ -2,45 +2,43 @@
  * Javascript code for the flip pattern described in dev_ressources/pattern_flip.html.
  */
 
-$(function() {
-  function flip_front(id) {
-    // the swapping is done in order for the function to also work with elements that have
-    // display: flex or table-row instead of simply display: block
-    const frontside = $('.flip-frontside[data-flipid="' + id + '"]');
-    const flipside = $('.flip-flipside[data-flipid="' + id + '"]');
-    const msg = $('.flip-flipside-msg[data-flipid="' + id + '"]');
+function flip_front(id) {
+  // the swapping is done in order for the function to also work with elements that have
+  // display: flex or table-row instead of simply display: block
+  const frontside = $('.flip-frontside[data-flipid="' + id + '"]');
+  const flipside = $('.flip-flipside[data-flipid="' + id + '"]');
+  const msg = $('.flip-flipside-msg[data-flipid="' + id + '"]');
 
-    if (flipside.css('display') != 'none') {
-      frontside.css('display', flipside.css('display'));
-      flipside.css('display', 'none');
-      msg.css('display', 'none');
-    }
+  if (flipside.css('display') != 'none') {
+    frontside.css('display', flipside.css('display'));
+    flipside.css('display', 'none');
+    msg.css('display', 'none');
   }
+}
 
-  function flip_flip(id) {
-    const frontside = $('.flip-frontside[data-flipid="' + id + '"]');
-    const flipside = $('.flip-flipside[data-flipid="' + id + '"]');
-    const msg = $('.flip-flipside-msg[data-flipid="' + id + '"]');
+function flip_flip(id) {
+  const frontside = $('.flip-frontside[data-flipid="' + id + '"]');
+  const flipside = $('.flip-flipside[data-flipid="' + id + '"]');
+  const msg = $('.flip-flipside-msg[data-flipid="' + id + '"]');
 
-    if (frontside.css('display') != 'none') {
-      flipside.css('display', frontside.css('display'));
-      frontside.css('display', 'none');
-      flipside.find('input').first().focus();
-    }
+  if (frontside.css('display') != 'none') {
+    flipside.css('display', frontside.css('display'));
+    frontside.css('display', 'none');
+    flipside.find('input').first().focus();
   }
+}
 
-  $(document).on('click', '.flip-frontbutton', function() {
+$(document).on('click', '.flip-frontbutton', function() {
+  flip_front(this.getAttribute('data-flipid'));
+  return false;
+});
+$(document).on('click', '.flip-flipbutton', function() {
+  flip_flip(this.getAttribute('data-flipid'));
+  return false;
+});
+$(document).on('keyup', '.flip-flipside', function(e) {
+  if (e.key === 'Escape') {
     flip_front(this.getAttribute('data-flipid'));
-    return false;
-  });
-  $(document).on('click', '.flip-flipbutton', function() {
-    flip_flip(this.getAttribute('data-flipid'));
-    return false;
-  });
-  $(document).on('keyup', '.flip-flipside', function(e) {
-    if (e.key === 'Escape') {
-      flip_front(this.getAttribute('data-flipid'));
-      e.stopPropagation();
-    }
-  });
+    e.stopPropagation();
+  }
 });
