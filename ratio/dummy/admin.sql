b69a05054cb4f8b95220bbe1ea29d06a2cfbdb68
Arne Kramer-Sunderbrink <herr-kramer-sunderbrink@t-online.de>
2021-03-31 19:29:06 +0200

    two new users in the dummy

--- ratio/dummy/admin.sql
+++ ratio/dummy/admin.sql
@@ -3,11 +3,13 @@
 INSERT INTO user (username, password)
 VALUES
   ('test', 'pbkdf2:sha256:50000$TCI4GzcX$0de171a4f4dac32e3364c7ddc7c14f3e2fa61f2d17574483f7ffbb431b4acb2f'),
-  ('other', 'pbkdf2:sha256:50000$kJPKsz6N$d2d4784f1b030a9761f5ccaeeaca413f27f2ecb76d6168407af962ddce849f79');
+  ('other', 'pbkdf2:sha256:50000$kJPKsz6N$d2d4784f1b030a9761f5ccaeeaca413f27f2ecb76d6168407af962ddce849f79'),
+  ('osanchez', 'pbkdf2:sha256:50000$TCI4GzcX$0de171a4f4dac32e3364c7ddc7c14f3e2fa61f2d17574483f7ffbb431b4acb2f'),
+  ('akramersunderbrink', 'pbkdf2:sha256:50000$TCI4GzcX$0de171a4f4dac32e3364c7ddc7c14f3e2fa61f2d17574483f7ffbb431b4acb2f');
 
 INSERT INTO subgraph (name, finished, deleted)
 VALUES
-  ('Dabigatran versus Warfarin in Patients with Atrial Fibrillation (2009)', 1, 0);
+  ('Dabigatran versus Warfarin in Patients with Atrial Fibrillation (2009)', 2, 0);
 
 INSERT INTO access (user_id, subgraph_id)
 VALUES
