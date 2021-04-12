15c5097d8281bb9f917d0ad2d4e6641d9eb9043c
Arne Kramer-Sunderbrink <herr-kramer-sunderbrink@t-online.de>
2021-04-12 23:49:27 +0200

    more dummy data

--- ratio/db.py
+++ ratio/db.py
@@ -68,6 +68,16 @@ def db_populate_dummy():
     from ratio.knowledge_model import get_subgraph_knowledge
     with current_app.open_resource('dummy/knowledge_1.ttl') as f:
         get_subgraph_knowledge(1).load_rdf_data(f, 'turtle')
+    with current_app.open_resource('dummy/knowledge_2.ttl') as f:
+        get_subgraph_knowledge(3).load_rdf_data(f, 'turtle')
+    with current_app.open_resource('dummy/knowledge_3.ttl') as f:
+        get_subgraph_knowledge(4).load_rdf_data(f, 'turtle')
+    with current_app.open_resource('dummy/knowledge_4.ttl') as f:
+        get_subgraph_knowledge(5).load_rdf_data(f, 'turtle')
+    with current_app.open_resource('dummy/knowledge_5.ttl') as f:
+        get_subgraph_knowledge(6).load_rdf_data(f, 'turtle')
+    with current_app.open_resource('dummy/knowledge_6.ttl') as f:
+        get_subgraph_knowledge(7).load_rdf_data(f, 'turtle')
 
     with current_app.open_resource('dummy/empty_subgraph_template.ttl') as f:
         with open(current_app.config['EMPTY_SUBGRAPH_TEMPLATE'], 'wb+') as f2:
--- ratio/dummy/admin.sql
+++ ratio/dummy/admin.sql
@@ -7,8 +7,19 @@ VALUES
 
 INSERT INTO subgraph (name, finished, deleted)
 VALUES
-  ('Dabigatran versus Warfarin in Patients with Atrial Fibrillation (2009)', 1, 0);
+  ('RELY-Connolly(2009)', 1, 0),
+  ('nothing', 0, 1),
+  ('ROCKET-AF-Patel(2011)', 1, 0),
+  ('ARISTOTLE-Granger(2011)', 1, 0),
+  ('ENGAGE-Giugliano(2013)', 1, 0),
+  ('ERISTA-AF-Rutherford(2020)', 0, 0),
+  ('ENSURE-AF-Goette(2016)', 1, 0);
 
 INSERT INTO access (user_id, subgraph_id)
 VALUES
-  (1, 1);
+  (1, 1),
+  (1, 3),
+  (1, 4),
+  (1, 5),
+  (1, 6),
+  (1, 7);
