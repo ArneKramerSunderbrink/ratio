b4ebfa4fde140088d6546d8df7f725cc84c764d2
Arne Kramer-Sunderbrink <herr-kramer-sunderbrink@t-online.de>
2021-06-30 14:16:58 +0200

    german version of the call

--- ratio/call.py
+++ ratio/call.py
@@ -1,11 +1,23 @@
 # call for participants
 
 from flask import Blueprint
+from flask import redirect
 from flask import render_template
+from flask import url_for
 
 bp = Blueprint('call', __name__, url_prefix='/call')
 
 
 @bp.route('')
 def call():
-    return render_template('call.html')
+    return redirect(url_for('call.call_de'))
+
+
+@bp.route('en')
+def call_en():
+    return render_template('call_en.html')
+
+
+@bp.route('de')
+def call_de():
+    return render_template('call_de.html')
